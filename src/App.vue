4dffc00601a155b53792099df9e09ceebde56d04
MARM <seanbrucet@gmail.com>
2019-06-19 15:46:19 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Sheep />
+    <h1>Beep Beep I'm a Sheep</h1>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Sheep from './components/sheep'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Sheep
   }
 }
 </script>
 
 <style>
+body, html {
+  height:100%;
+  margin:0;
+  padding:0;
+  background:#eee;
+  display:flex; 
+  flex-direction: column;
+}
+
 #app {
+  flex:1;
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+svg {
+  margin: 0 auto;
+}
+
+h1 {
+  margin:0;
 }
 </style>
